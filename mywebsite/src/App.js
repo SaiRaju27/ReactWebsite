467c6264ca0e46967bda22f84735b53e2c798c9e
SaiRaju27 <kraju9589@gmail.com>
2022-07-25 12:39:36 +0530

    Created React App

---
+++ mywebsite/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Service from './Service';
+
+const App = () =>{
+  return (
+
+    <>
+    <Home/>
+    <About/>
+    <Service/>
+    <Contact/>
+    </>
+  )
+}
+
+export default App;
